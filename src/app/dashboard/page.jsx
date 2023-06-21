26576adb145d8f080a489142d380bc27bde244dc
adeeep14 <disconetct@gmail.com>
2023-06-21 20:37:41 +0700

    old way to fetch datas

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  //OLD WAY TO FETCH DATA
+
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //       cache: "no-cache",
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+
   return <div>Dashboard</div>;
 };
 
